866709205b7270fb3a0f57967745030851543ff4
Reynaldo Perez Pauli <reynaldoperezpauli6@gmail.com>
2025-10-12 15:17:19 -0500

    U

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import { useLanguage } from "../context/LanguageContext";
+
 export default function Footer() {
+    const {lang} = useLanguage();
+
     return (
       <footer>
-        <p>© 2025 Fresh Fade Society Site by Reynaldo Perez Pauli. All Rights Reserved.</p>
+        <p>{lang==="en" ? "© 2025 Fresh Fade Society Site by Reynaldo Perez Pauli. All Rights Reserved." : "© 2025 Fresh Fade Society Site por Reynaldo Perez Pauli. Todods los Derechos Reservados."}</p>
       </footer>
     );
   }
